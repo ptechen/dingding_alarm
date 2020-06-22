4f9a8822f0e1646b3a16f05ee4b024eba685a7a8
petechen <15736755067@163.com>
2020-06-22 18:34:01 +0800

    update

--- alarm/alarm_test.go
+++ alarm/alarm_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestAlarm_SendAlarm(t *testing.T) {
 	p := &Alarm{
-		Url: "https://oapi.dingtalk.com/robot/send?access_token=d7cae6bf6a69bc327b8c69696cb0c6ea08d6cef645891e41aee7715651961f8e",
+		Url: "https://oapi.dingtalk.com/robot/send?access_token=***",
 		Message: &Message{
 			Msgtype: "text",
 			Text:    &Text{
