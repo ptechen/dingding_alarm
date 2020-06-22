5dc322880fec11f7efbec3fb319c387239a8fc55
petechen <15736755067@163.com>
2020-06-22 19:20:17 +0800

    update

--- alarm/alarm.go
+++ alarm/alarm.go
@@ -5,43 +5,6 @@ import (
 	"github.com/kirinlabs/HttpRequest"
 )
 
-type Alarm struct {
-	BotName string   `json:"bot_name"`
-	GroupAt string   `json:"group_at"`
-	Url     string   `json:"url"`
-	Secret  string   `json:"secret"`
-	Message *Message `json:"message"`
-}
-
-type Message struct {
-	Msgtype  string    `json:"msgtype"`
-	Text     *Text     `json:"text"`
-	Link     *Link     `json:"link"`
-	Markdown *Markdown `json:"markdown"`
-	At       *At       `json:"at"`
-}
-
-type At struct {
-	AtMobiles []string `json:"atMobiles"`
-	IsAtAll   bool     `json:"isAtAll"`
-}
-
-type Text struct {
-	Content string `json:"content"`
-}
-
-type Link struct {
-	Text       string `json:"text"`
-	Title      string `json:"title"`
-	PicUrl     string `json:"pic_url"`
-	MessageUrl string `json:"messageUrl"`
-}
-
-type Markdown struct {
-	Title string `json:"title"`
-	Text  string `json:"text"`
-}
-
 func (p *Alarm) SendAlarm() (err error) {
 	data := make(map[string]interface{})
 	dataBytes, _ := json.Marshal(p.Message)
