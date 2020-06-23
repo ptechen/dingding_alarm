c2a4b2ba0598d5d1019de95d6367b7a43ab03e6b
petechen <15736755067@163.com>
2020-06-23 19:15:25 +0800

    update

--- bot/bot.go
+++ bot/bot.go
@@ -8,7 +8,7 @@ import (
 //go:generate kratos tool protoc --grpc bot.proto
 
 func (p *Bot) SendAlarm() (err error) {
-	if p.IsAtAll && p.Message.At != nil{
+	if p.IsAtAll && p.Message.At != nil {
 		p.Message.At.IsAtAll = true
 	}
 	data := make(map[string]interface{})
@@ -20,5 +20,21 @@ func (p *Bot) SendAlarm() (err error) {
 	req := HttpRequest.NewRequest()
 	req = req.SetHeaders(map[string]string{"Content-Type": "application/json"})
 	_, err = req.Post(p.Url, data)
+	return err
+}
+
+type ParamsBot struct {
+	BotName map[string]string `json:"bot_name"`
+	GroupAt map[string]*At    `json:"group_at"`
+}
+
+func (p *Bot) AlarmConfig(params *ParamsBot) {
+	p.Url = params.BotName[p.BotName]
+	p.Message.At, _ = params.GroupAt[p.GroupAt]
+}
+
+func (p *Bot) Send(params *ParamsBot) (err error) {
+	p.AlarmConfig(params)
+	err = p.SendAlarm()
 	return err
 }
